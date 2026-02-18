0b7d0334bb160a7cfd9d8ead2d2b054b2819a491
Michael Olufade <michaelprince157@gmail.com>
2026-02-17 21:02:05 -0700

    Part 1

---
+++ part1/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+// import { jsxDEV } from 'react/jsx-dev-runtime'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
